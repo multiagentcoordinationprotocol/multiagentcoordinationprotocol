db6a9d955000c9b18b30f111e3800d28fbfcf29d
Ajit Koti <ajitkoti@zer07labs.com>
2026-04-09 16:17:07 -0700

    Fix Swift GRPC Issue

--- packages/proto-swift/Package.swift
+++ packages/proto-swift/Package.swift
@@ -12,14 +12,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.0"),
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.24.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "MACPProto",
             dependencies: [
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "GRPCCore", package: "grpc-swift"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
             ],
             path: "Sources/MACPProto"
         ),
